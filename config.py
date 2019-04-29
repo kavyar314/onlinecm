4d122a7adda11f0b6461df3dacca88ad06f0d7dc
Kavya Ravichandran <rkavya@mit.edu>
2019-04-28 21:10:12 -0400

    changes in params

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
-n_heavy_hitters = 20
+n_heavy_hitters = 40
 
 time_between_train = 10
 
-f_list = [1, 0.9, 0.75, 0.5,  0.3, 0.25, 0.1]
+f_list = [1, 0.99, 0.9, 0.75, 0.5,  0.3, 0.25, 0.1]
 
 decay = f_list[1]
 
 n_gradient_updates = 1
 
-len_stream = None
+len_stream = 1000
 
 half_batch=16
 
