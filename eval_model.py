784fcc62bf3d68d9b72002f574969eb67452561c
Kavya Ravichandran <rkavya@mit.edu>
2019-05-11 16:35:53 -0400

    calls dataloader with dataset and also returns mapping from word to embedding

--- eval_model.py
+++ eval_model.py
@@ -4,12 +4,12 @@ import config
 
 import numpy as np
 
-def evaluate_model(model, full=True, verbose=False):
+def evaluate_model(model, dataset, full=True, verbose=False):
 	'''
 	:param full: boolean. True if full stream, if False, then start evaluation from where training stream ended (config.len_stream)
 	'''
 	# make the lookup table
-	table_for_eval = gen_lookup_table(full=full, verbose=verbose)
+	table_for_eval = gen_lookup_table(dataset, full=full, verbose=verbose)
 	hh_x, _ = table_for_eval.sample_elements(hh=True,n_samples=200)
 	not_hh_x, _ = table_for_eval.sample_elements(hh=False,n_samples=20000)
 
@@ -22,15 +22,15 @@ def evaluate_model(model, full=True, verbose=False):
 	return correct_hh[0], len(pred_y_hh), correct_not_hh[0], len(pred_y_not_hh)
 
 
-def gen_lookup_table(full, verbose):
+def gen_lookup_table(dataset, full, verbose):
 	oracle_table = lookup_table_oracle.lookup_table(decay=1, reservoir=False) # will keep track of all the elements
 
 	if full:
-		data = data_loader.load(amount=None, verbose=verbose)
+		raw_data, data = data_loader.load(dataset, amount=None, verbose=verbose)
 	else:
-		data = data_loader.load(amount=-1, start=config.len_stream, verbose=verbose)
+		raw_data, data = data_loader.load(dataset, amount=-1, start=config.len_stream, verbose=verbose)
 
-	for d in data:
+	for d in raw_data:
 		oracle_table.increment_count(tuple(d))
-	return oracle_table
+	return oracle_table, dict(zip(raw_data, data))
 		
