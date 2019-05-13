f6079d2c292067e6430c336c006735416ef41d95
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 19:34:27 -0400

    more training data; more training

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ time_between_train = 100 # so that you may actually have that many
 
 f_list = [1, 0.99, 0.9, 0.75, 0.5,  0.3, 0.25, 0.1]
 
-decay = f_list[1]
+decay = f_list[0]
 
-n_gradient_updates = 4
+n_gradient_updates = 10
 
-len_stream = 1000
+len_stream = 6000
 
 half_batch=16
 
