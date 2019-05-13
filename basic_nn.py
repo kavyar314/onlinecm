5c604ebb5849a34b71198f7abe1a31f71c26be22
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 19:33:08 -0400

    changes to architecture and activations

--- basic_nn.py
+++ basic_nn.py
@@ -11,17 +11,20 @@ def model(n_layers=config.n_layers):
 		model.add(Dense(100, activation='relu'))
 
 	model.add(Dense(50, activation='relu'))
-	model.add(Dense(10, activation='relu'))
-	model.add(Dense(1, activation='linear'))
+	model.add(Dense(10, activation='linear'))
+	model.add(Dense(1, activation='sigmoid'))
 
 	model.compile(loss='mean_squared_error', optimizer='sgd')
 
 	return model
 
 
-def lstm_model(n_units=256, n_layers=1):
+def lstm_model(n_units=512, n_layers=5):
 	model = Sequential()
 	model.add(LSTM(n_units, input_shape=(config.trun_len, config.trun_len)))
+
+	for _ in range(n_layers-1):
+		model.add(Dense(100, activation='relu'))
 
 	model.add(Dense(50, activation='relu'))
 	model.add(Dense(10, activation='linear'))
