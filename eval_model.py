2bb4bcf40289cbe9c7d6c9afadcd72a9f051b988
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 16:32:13 -0400

    takes smaller sample for eval

--- eval_model.py
+++ eval_model.py
@@ -4,12 +4,12 @@ import config
 
 import numpy as np
 
-def evaluate_model(model, dataset, full=True, verbose=False):
+def evaluate_model(model, dataset, amount=4000, verbose=False):
 	'''
 	:param full: boolean. True if full stream, if False, then start evaluation from where training stream ended (config.len_stream)
 	'''
 	# make the lookup table
-	table_for_eval = gen_lookup_table(dataset, full=full, verbose=verbose)
+	table_for_eval = gen_lookup_table(dataset, amount=amount, verbose=verbose)
 	hh_x, _ = table_for_eval.sample_elements(hh=True,n_samples=200)
 	not_hh_x, _ = table_for_eval.sample_elements(hh=False,n_samples=20000)
 
@@ -22,13 +22,10 @@ def evaluate_model(model, dataset, full=True, verbose=False):
 	return correct_hh[0], len(pred_y_hh), correct_not_hh[0], len(pred_y_not_hh)
 
 
-def gen_lookup_table(dataset, full, verbose):
+def gen_lookup_table(dataset, amount, verbose):
 	oracle_table = lookup_table_oracle.lookup_table(decay=1, reservoir=False) # will keep track of all the elements
 
-	if full:
-		raw_data, data = data_loader.load(dataset, amount=None, verbose=verbose)
-	else:
-		raw_data, data = data_loader.load(dataset, amount=-1, start=config.len_stream, verbose=verbose)
+	raw_data, data = data_loader.load(dataset, amount=amount, verbose=verbose)
 
 	for d in raw_data:
 		oracle_table.increment_count(tuple(d))
