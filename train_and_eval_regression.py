d61cc730006483a45b8e31ca9190fc3cb88a508c
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 16:50:05 -0400

    fixed variable names

--- train_and_eval_regression.py
+++ train_and_eval_regression.py
@@ -2,7 +2,7 @@ import trainer, eval_model
 
 if __name__ == '__main__':
 	model, params = trainer.train('aol')
-	pp_f, ap_f, pn_f, an_f = eval_model.evaluate_model(model, 'aol')
-	write_string = "aol, %d, %d, %d, %d" % (pp_f, ap_f, pn_f, an_f)
+	ep_hh, ep_not_hh, k_hh, k_not_hh = eval_model.evaluate_model(model, 'aol')
+	write_string = "aol, %04f, %04f, %04f, %04f" % (ep_hh, ep_not_hh, k_hh, k_not_hh)
 	with open('regression_results.csv', 'a') as f:
 		f.write(write_string)
