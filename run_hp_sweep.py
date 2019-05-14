140de251868a107f04e1c56e75102fd566a24adc
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 20:52:15 -0400

    also n_layers

--- run_hp_sweep.py
+++ run_hp_sweep.py
@@ -5,23 +5,25 @@ n_samples = [20, 40, 50, 60]
 time_between_train = [50, 75, 100, 200, 500, 1000]
 f_list = config.f_list
 n_gradient_updates = [1, 2, 3, 4, 10, 20]
+n_layers = [1,2,3,4]
 
 outfile = "aol_parameter_sweep.csv"
 # outfile
 
 def hp_sweep():
-	for h in n_samples:
-		for t in time_between_train:
-			for decay in f_list:
-				for ep in n_gradient_updates:
-					model, params = trainer.train('aol', n_samples=h, epochs=ep, n_heavy_hitters=h, decay=decay, n_before_update=t)
-					print("completed:", params)
-					hh_within_e4, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.0001)
-					hh_within_e3, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.001)
-					hh_within_e2, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.01)
-					write_string = "%d, %d, %02f, %d, %04f, %04f, %04f, %04f\n" % (h, t, decay, ep, hh_within_e4, not_hh_within_e4, hh_within_e3, not_hh_within_e4, hh_within_e2, not_hh_within_e4)
-					with open(outfile, 'a') as f:
-						f.write(write_string)
+	for lay in n_layers:
+		for h in n_samples:
+			for t in time_between_train:
+				for decay in f_list:
+					for ep in n_gradient_updates:
+						model, params = trainer.train('aol', n_samples=h, epochs=ep, n_heavy_hitters=h, decay=decay, n_before_update=t, n_layers=lay)
+						print("completed:", params)
+						hh_within_e4, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.0001)
+						hh_within_e3, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.001)
+						hh_within_e2, not_hh_within_e4 = eval_model.evaluate_model(model, 'aol', eps=0.01)
+						write_string = "%d, %d, %d, %02f, %d, %04f, %04f, %04f, %04f\n" % (lay, h, t, decay, ep, hh_within_e4, not_hh_within_e4, hh_within_e3, not_hh_within_e4, hh_within_e2, not_hh_within_e4)
+						with open(outfile, 'a') as f:
+							f.write(write_string)
 
 
 	# train(verbose=False, n_samples=config.half_batch, epochs=config.n_gradient_updates, 
