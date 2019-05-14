23e861e33f888952e71a66008125b4435cf8bd27
Kavya Ravichandran <rkavya@mit.edu>
2019-05-13 20:52:56 -0400

    fewer decays and more samples

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
 n_heavy_hitters = 20 # make this smaller
 
-time_between_train = 100 # so that you may actually have that many
+time_between_train = 1000 # so that you may actually have that many
 
-f_list = [1, 0.99, 0.9, 0.75, 0.5,  0.3, 0.25, 0.1]
+f_list = [1, 0.99, 0.9, 0.75]#, 0.5,  0.3, 0.25, 0.1]
 
 decay = f_list[0]
 
 n_gradient_updates = 10
 
-len_stream = 6000
+len_stream = 10000
 
 half_batch=16
 
