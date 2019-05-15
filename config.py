c550d586cba8d3ead8cf1422cab29d65d5d9b11b
Kavya Ravichandran <rkavya@mit.edu>
2019-05-15 17:07:36 -0400

    params

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
-n_heavy_hitters = 20 # make this smaller
+n_heavy_hitters = 60 # make this smaller
 
-time_between_train = 1000 # so that you may actually have that many
+time_between_train = 200 # so that you may actually have that many
 
 f_list = [1, 0.99, 0.9, 0.75]#, 0.5,  0.3, 0.25, 0.1]
 
 decay = f_list[0]
 
-n_gradient_updates = 10
+n_gradient_updates = 20
 
 len_stream = 10000
 
