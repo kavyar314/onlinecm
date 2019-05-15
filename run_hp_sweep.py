7b47221a608419126172d495b0e7745e73bf2516
Kavya Ravichandran <rkavya@mit.edu>
2019-05-15 16:31:25 -0400

    fixed for rnn

--- run_hp_sweep.py
+++ run_hp_sweep.py
@@ -5,23 +5,23 @@ n_samples = [20, 40, 50, 60]
 time_between_train = [200]
 f_list = config.f_list
 n_gradient_updates = [1, 2, 3, 4, 10, 20]
-n_layers = [4, 3]
+# n_layers = [4, 3]
 
-outfile = "aol_parameter_sweep_morespecific.csv"
+outfile = "aol_parameter_sweep_rnn.csv"
 # outfile
 
 def hp_sweep():
-	for lay in n_layers:
-		for h in n_samples[::-1]:
-			for t in time_between_train[::-1]:
-				for decay in f_list:
-					for ep in n_gradient_updates:
-						model, params = trainer.train('aol', n_samples=h, epochs=ep, n_heavy_hitters=h, decay=decay, n_before_update=t, n_layers=lay)
-						print("completed:", params)
-						[hh_within_e4, hh_within_e3, hh_within_e2],[not_hh_within_e4, not_hh_within_e3, not_hh_within_e2] = eval_model.evaluate_model(model, 'aol', eps_list=[0.0001, 0.001, 0.01])
-						write_string = "%d, %d, %d, %02f, %d, %04f, %04f, %04f, %04f, %04f, %04f\n" % (lay, h, t, decay, ep, hh_within_e4, not_hh_within_e4, hh_within_e3, not_hh_within_e3, hh_within_e2, not_hh_within_e2)
-						with open(outfile, 'a') as f:
-							f.write(write_string)
+	# for lay in n_layers:
+	for h in n_samples[::-1]:
+		for t in time_between_train[::-1]:
+			for decay in f_list:
+				for ep in n_gradient_updates:
+					model, params = trainer.train('aol', n_samples=h, epochs=ep, n_heavy_hitters=h, decay=decay, n_before_update=t)#, n_layers=lay)
+					print("completed:", params)
+					[hh_within_e4, hh_within_e3, hh_within_e2],[not_hh_within_e4, not_hh_within_e3, not_hh_within_e2] = eval_model.evaluate_model(model, 'aol', eps_list=[0.0001, 0.001, 0.01])
+					write_string = "%d, %d, %02f, %d, %04f, %04f, %04f, %04f, %04f, %04f\n" % (h, t, decay, ep, hh_within_e4, not_hh_within_e4, hh_within_e3, not_hh_within_e3, hh_within_e2, not_hh_within_e2)
+					with open(outfile, 'a') as f:
+						f.write(write_string)
 
 
 	# train(verbose=False, n_samples=config.half_batch, epochs=config.n_gradient_updates, 
