2a29032e5cf2b6ef6d5d61edd4cb5ea9b42d4f1a
Kavya Ravichandran <rkavya@mit.edu>
2019-05-15 20:17:26 -0400

    no flush

--- trainer.py
+++ trainer.py
@@ -49,7 +49,7 @@ def train(dataset, verbose=False, n_samples=config.half_batch, epochs=config.n_g
 				print(loss, loss_thres, i)
 				if loss < loss_thres:
 					oracle.decay_n_heavy_hitters()
-				oracle.flush()
+				# oracle.flush()
 				#	fit for n_gradient_updates epochs
 				print(full_training_x.shape)
 				nn.fit(full_training_x, full_training_y, batch_size=full_training_x.shape[0], epochs=epochs, verbose=2)
